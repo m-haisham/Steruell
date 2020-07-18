78885ba93afbd78568d168025ff374d2b5c73860
mHaisham <mHaisham@users.noreply.github.com>
2020-07-19 01:12:42 +0500

    Rectangles created once

--- shapes/rectangle.py
+++ shapes/rectangle.py
@@ -6,12 +6,26 @@ from .shape import Shape
 
 class Rectangle(Shape):
     def __init__(self, position: Vector2D, size: Vector2D, color: Color):
-        self.position = position
+        self._position = position
         self.size = size
         self.color = color
+        self.size_color = color[:]
+
+        self.rect = pygame.Rect(
+            self._position.x, self._position.y,
+            self.size.x, self.size.y
+        )
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, value: Vector2D):
+        self._position = value
+
+        # move rect
+        self.rect.x = value.x
 
     def draw(self, surface):
-        pygame.draw.rect(surface, self.color, (
-            self.position.x, self.position.y,
-            self.size.x, self.size.y
-        ))
+        pygame.draw.rect(surface, self.color, self.rect)
